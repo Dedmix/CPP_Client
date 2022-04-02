605061dd2e9d796304e776a571a7f4bbc1df402e
Dedmix <84181853+Dedmix@users.noreply.github.com>
2022-04-02 09:32:42 +0500

    Add files via upload

---
+++ C++ client/ConsoleApplication1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+#include <list>
+#include <vector>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+std::list<SOCKET> ClientList;
+
+void Run(SOCKET);
+
+int main()
+{
+    WSADATA data;
+    int e = WSAStartup(MAKEWORD(2, 2), &data);
+    if (e != 0) {
+        std::cout << "";
+    }
+
+    SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ServerSocket < 0) {
+
+        std::cout << "Socket error1" << std::endl;
+        return 1;
+    }
+
+    struct sockaddr_in serverAdress;
+    memset(&serverAdress, 0x0, sizeof(serverAdress));
+    serverAdress.sin_family = AF_INET;
+    serverAdress.sin_port = htons(8080);
+    serverAdress.sin_addr.S_un.S_un_b.s_b1 = 127;
+    serverAdress.sin_addr.S_un.S_un_b.s_b2 = 0;
+    serverAdress.sin_addr.S_un.S_un_b.s_b3 = 0;
+    serverAdress.sin_addr.S_un.S_un_b.s_b4 = 1;
+    int result = bind(
+        ServerSocket,
+        (const sockaddr*)(&serverAdress),
+        sizeof(serverAdress)
+    );
+    if (result == SOCKET_ERROR) {
+
+        std::cout << "Socket error2" << std::endl;
+        return 1;
+    }
+    result = listen(ServerSocket, 100);
+    if (result < 0) {
+        std::cout << "Socket error3" << std::endl;
+        return 1;
+    }
+    while (true) {
+        std::cout << "wait client connection\n";
+        SOCKET clientSocket = accept(ServerSocket, nullptr, nullptr);
+        std::cout << "a new client connected\n";
+        new std::thread(Run, clientSocket);
+        ClientList.push_back(clientSocket);
+    }
+    return 0;
+}
+void Run(SOCKET s) {
+    char message[512];
+    int n = recv(s, message, 512, 0);
+    if (n < 0) {
+        return;
+    }
+    std::cout << message << std::endl;
+    send(s, "pnog", 5, 0);
+    if (n < 0) {
+        return;
+    }
+    while (true) {
+        int n = recv(s, message, 512, 0);
+        if (n < 0) {
+            return;
+        }
+    }
+   // ClientList.remove(s);
+}
---
+++ Client/Client.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+#include <list>
+#include <vector>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+std::list<SOCKET> ClientList;
+void Run(SOCKET);
+
+int main()
+{
+    WSADATA data;
+    int e = WSAStartup(MAKEWORD(2, 2), &data);
+    if (e != 0) {
+        std::cout << "";
+        return 1;
+    }
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == SOCKET_ERROR) {
+        std::cout << "Socket error 1.1" << std::endl;
+        return 1;
+    }
+    
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.S_un.S_un_b.s_b1 = 127;
+    addr.sin_addr.S_un.S_un_b.s_b2 = 0;
+    addr.sin_addr.S_un.S_un_b.s_b3 = 0;
+    addr.sin_addr.S_un.S_un_b.s_b4 = 1;
+
+    e = connect(clientSocket, (sockaddr*)(&addr), sizeof(addr));
+    if (e != 0) {
+        std::cout << "Socket error 1.2" << std::endl;
+        return 1;
+    }
+
+   // e=connect(clie)
+    int n = send(clientSocket, "Ping", 5, 0);
+    if (n < 0) {
+        std::cout << "socket error 1.3\n";
+        return 1;
+    }
+    char message[512];
+    memset(message, 0, 512);
+
+    n = recv(clientSocket, message, 512, 0);
+    if (n < 0) {
+        std::cout << "Socket error 1.4" << std::endl;
+        return 1;
+    }
+    std::cout << message << std::endl;
+
+  
+}
